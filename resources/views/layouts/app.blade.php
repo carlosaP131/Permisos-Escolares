ab5f6bb41c4c3644cf32374bc7fc62c9d5269595
Skar13F <of5162@gmail.com>
2024-01-18 11:26:14 -0600

    merge login with master

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,15 +13,15 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
-  
 
 
-<!-- Bootstrap CSS -->
-<link  href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-<!-- Font Awesome CSS -->
-<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
-    
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
+
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
@@ -32,12 +32,14 @@
     <link href="{{ asset('css/secretaria.css') }}" rel="stylesheet">
     <script src="{{ asset('js/secretaria.js') }}"></script>
 
-    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet"> 
+    <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
 
 
 
 
-  <!-- plantilla -->
+
+
+    <!-- plantilla -->
 
     <script src="{{ asset('js/jquery.min.js') }}"></script>
     <script src="{{ asset('js/bootstrap.min.js') }}"></script>
@@ -53,14 +55,6 @@
     <div id="app">
 
         @guest
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ms-auto" style="align-items: center">
-                @if (Route::has('login'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                    </li>
-                @endif
-            </ul>
         @else
             <!-- Section to show views and content -->
             <main class="">
@@ -68,14 +62,14 @@
             </main>
         @endguest
         <!-- Section to show login-->
-        <main class="py-4">
+        <main class="">
             @yield('login')
         </main>
     </div>
     <!-- Scripts -->
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </body>
 
