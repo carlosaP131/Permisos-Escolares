33b6e404d06dc1c32d5b5dfdd15cae4390ae47d1
Skar13F <101080281+Skar13F@users.noreply.github.com>
2024-01-18 11:47:47 -0600

    Agregado de tabla para profesores (#9)
    * Tabla alumnos exportar pdf
    Co-authored-by: Getze1221 <alebk201@gmail.com>

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('auth.login');
+    return view('tabla');
+});
+
+Route::get('/tabla', function () {
+    return view('tabla');
 });
 Route::get('/alumnos', function () {
     return view('secretaria.alumnos');
