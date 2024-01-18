44388d1b183585e2173f60b42fd8b50710462b94
Skar13F <101080281+Skar13F@users.noreply.github.com>
2024-01-17 22:01:43 -0600

    Diseño de login (#2)
    * agregado de nuevos achivos
    * se renombraron algunos archivos
    * Diseño del login
    
    Co-authored-by: Niky11 <nancyobed11@gmail.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,19 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+                <div class="card-header text-center">
+                <h4>
+                  <img src="{{ asset('imagen/logo.png') }}"  style="width: 200px; height: 200px; margin-right: 10px;">
+    </h4>
+                </div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-end">
+                                <i class="fa fa-user-circle" style="color: #800000;"></i> {{ __('Usuario') }}
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                <input id="email" type="email" placeholder="Ingrese su correo electrónico" class="form-control rounded @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,10 +32,12 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-end">
+                                <i class="fas fa-lock" style="color: #800000;"></i> {{ __('Contraseña') }}
+                            </label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" placeholder="Ingrese su contraseña" class="form-control rounded @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -53,8 +61,8 @@
 
                         <div class="row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                <button type="submit" class="btn btn-info rounded">
+                                    <i class="fas fa-sign-in-alt"></i> {{ __('Ingresar') }}
                                 </button>
 
                                 @if (Route::has('password.request'))
