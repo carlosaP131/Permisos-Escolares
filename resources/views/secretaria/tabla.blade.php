6b4258c057c0a23146af4f61973a2ea858a4404a
Skar13F <of5162@gmail.com>
2024-01-19 16:01:01 -0600

    Scripts organizados para funcionalidad

--- resources/views/secretaria/tabla.blade.php
+++ resources/views/secretaria/tabla.blade.php
@@ -506,7 +506,14 @@
                 </div>
             </div>
         </div>
+    <!-- tambien para las tablas -->
+    <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+    <script src="{{ asset('js/datatables.min.js') }}"></script>
+    <script src="{{ asset('js/pdfmake.min.js') }}"></script>
 
-      
+    <script src="{{ asset('js/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('js/custom.js') }}"></script>
+
     </body>
 @endsection
