942c3d0b00793081a64eb9d0f2abf85e1666fc18
Carlos <carlos.cantrax303@gmail.com>
2024-01-22 11:41:27 -0600

    se le dio formato a los formularios

--- resources/views/secretaria/actualizarpermiso.blade.php
+++ resources/views/secretaria/actualizarpermiso.blade.php
@@ -1,7 +1,7 @@
 @extends('home')
 @section('main')
     {{-- Form para generar un permiso --}}
-    <div class="container p-5 my-5 border bg-dark text-white">{{-- Inicia el contenedor donde se almacenaran todos los elementos del form --}}
+    <div class="formulario-permiso container p-5 my-5  text-dark">{{-- Inicia el contenedor donde se almacenaran todos los elementos del form --}}
         <h2>Actualizar Permiso</h2>
         <form action="{{ route('actualizar-permiso', ['idPermiso' => $permiso->id]) }}" method="POST">
             @csrf
@@ -39,34 +39,44 @@
                     <div class="col">
                         <label for="nombre">Nombre</label>
                         <input type="text" class="form-control" placeholder="Ingrese el nombre" name="nombre"
-                            value="{{ $alumno->nombre }}">
+                            value="{{ $alumno->nombre }}" readonly>
                     </div>
                     <div class="col">
                         <label for="matricula">Matricula</label>
                         <input type="text" class="form-control" placeholder="Ingrese la Matricula" name="matricula"
-                            value="{{ $alumno->matricula }}">
+                            value="{{ $alumno->matricula }}" readonly>
                     </div>
                 </div>
             </div>{{-- Termina div de los inputs nombre y matricula --}}
             <div class="mb-3">{{-- Inicia div del select carrera tiene 8 opciones --}}
+                <div class="row">
+                    <div class="col">
+                        <label for="sel1" class="form-label">Carrera:</label>
+                        <select class="form-select " id="carrera" name="carrera" disabled>
+                            <option>{{ $alumno->carrera }}</option>
 
-                <label for="sel1" class="form-label">Carrera:</label>
-                <select class="form-select" id="carrera" name="carrera">
-                    <option>{{ $alumno->carrera }}</option>
-
-                </select>
+                        </select>
+                    </div>
+                    <div class="col">
+                        {{-- Inicia div del select semestre las opciones seran dinamicas dependiendo del ciclo escolar seleccionado --}}
+                        <label for="sel1" class="form-label">Grupo:</label>
+                        <select class="form-select " id="grupo" name="grupo" disabled>
+                            <option>{{ $alumno->semestre . $alumno->grupo }}</option>
+                        </select>
+                        {{-- Termina el div del select semestre --}}
+                    </div>
             </div>{{-- Termina div del select carrera --}}
             <div class="mb-3">{{-- Inicia el div de la seccion de seleccion del año y de el ciclo escolar --}}
                 <div class="row">
                     <div class="col">
                         <label for="Semestre_año">Año</label>
                         <input type="text" class="form-control" placeholder="Ingrese el semestre" name="Semestre_anio"
-                            value="{{ date('Y') }}">
+                            value="{{ date('Y') }}" readonly>
 
                     </div>
                     <div class="col">
                         <label for="sellist1">Ciclo</label>
-                        <select class="form-select" id="Semestre_tipo" name="semestre">
+                        <select class="form-select" id="Semestre_tipo" name="semestre" disabled>
                             <option>A</option>
                             <option>B</option>
 
@@ -74,12 +84,7 @@
                     </div>
                 </div>
             </div>{{-- Termina select de la seccion de año y ciclo --}}
-            <div class="mb-3 mt-3">{{-- Inicia div del select semestre las opciones seran dinamicas dependiendo del ciclo escolar seleccionado --}}
-                <label for="sel1" class="form-label">Semestre:</label>
-                <select class="form-select" id="grupo" name="grupo">
-                    <option>{{ $alumno->semestre }}</option>
-                </select>
-            </div>{{-- Termina el div del select semestre --}}
+
             <div class="mb-3 mt-3" id="especialidadContainer" style="display: none;">{{-- Inicia div de la seleccion del grupo esta seccion estara oculta solo
                                                                                              solo se mostrara si en carrera se selecciona Enfermeria Odontologia u Medicina --}}
                 <label for="especialidad" class="form-label">Grupo</label>
