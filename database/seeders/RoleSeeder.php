73d16c66bef13b26bd627748f2618e6494b23545
Skar13F <101080281+Skar13F@users.noreply.github.com>
2024-01-27 17:15:42 -0600

    Agregado de superAdmin (#34)

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -17,31 +17,32 @@ class RoleSeeder extends Seeder
                 /**
                  * Se crean los roles
                  */
-                $role1 = Role::create(['name' => 'Admin']);
-                $role2 = Role::create(['name' => 'Secretaria']);
-                $role3 = Role::create(['name' => 'Profesor']);
+                $role1 = Role::create(['name' => 'SuperAdmin']);
+                $role2 = Role::create(['name' => 'Admin']);
+                $role3 = Role::create(['name' => 'Secretaria']);
+                $role4 = Role::create(['name' => 'Profesor']);
                 /**
                  * Se crean los permisos hacia las rutas o vistas y se le asigna
                  * los permisos a cada role
                  */
-                Permission::create(['name' => 'home'])->syncRoles([$role1, $role2, $role3]);
+                Permission::create(['name' => 'home'])->syncRoles([$role1, $role2, $role3, $role4]);
 
-                Permission::create(['name' => 'alumno-inicio'])->syncRoles([$role1, $role2]);
-                Permission::create(['name' => 'alumno-permisos'])->syncRoles([$role1, $role2, $role3]);
-                Permission::create(['name' => 'administrador-usuarios'])->syncRoles([$role1]);
+                Permission::create(['name' => 'alumno-inicio'])->syncRoles([$role1, $role2, $role3]);
+                Permission::create(['name' => 'alumno-permisos'])->syncRoles([$role1, $role2, $role3, $role4]);
+                Permission::create(['name' => 'administrador-usuarios'])->syncRoles([$role1, $role2]);
 
-                Permission::create(['name' => 'formulario-permiso'])->syncRoles([$role1, $role2]);
-                Permission::create(['name' => 'crear-permiso'])->syncRoles([$role1, $role2]);
-                Permission::create(['name' => 'vista-permiso'])->syncRoles([$role1, $role2]);
-                Permission::create(['name' => 'actualizar-permiso'])->syncRoles([$role1, $role2]);
-                Permission::create(['name' => 'permiso-destroy'])->syncRoles([$role1]);
+                Permission::create(['name' => 'formulario-permiso'])->syncRoles([$role1, $role2,$role3]);
+                Permission::create(['name' => 'crear-permiso'])->syncRoles([$role1, $role2, $role3]);
+                Permission::create(['name' => 'vista-permiso'])->syncRoles([$role1, $role2, $role3]);
+                Permission::create(['name' => 'actualizar-permiso'])->syncRoles([$role1, $role2, $role3]);
+                Permission::create(['name' => 'permiso-destroy'])->syncRoles([$role1, $role2]);
 
-                Permission::create(['name' => 'crear-usuario'])->syncRoles([$role1]);
-                Permission::create(['name' => 'actualizar-usuario'])->syncRoles([$role1]);
-                Permission::create(['name' => 'usuarios-destroy'])->syncRoles([$role1]);
+                Permission::create(['name' => 'crear-usuario'])->syncRoles([$role1,$role2]);
+                Permission::create(['name' => 'actualizar-usuario'])->syncRoles([$role1,$role2]);
+                Permission::create(['name' => 'usuarios-destroy'])->syncRoles([$role1, $role2]);
 
-                Permission::create(['name' => 'vista-cargar-excel'])->syncRoles([$role1]);
-                Permission::create(['name' => 'poblar-alumnos'])->syncRoles([$role1]);
-                Permission::create(['name' => 'borrar-alumnos'])->syncRoles([$role1]);
+                Permission::create(['name' => 'vista-cargar-excel'])->syncRoles([$role1, $role2]);
+                Permission::create(['name' => 'poblar-alumnos'])->syncRoles([$role1, $role2]);
+                Permission::create(['name' => 'borrar-alumnos'])->syncRoles([$role1, $role2]);
         }
 }
