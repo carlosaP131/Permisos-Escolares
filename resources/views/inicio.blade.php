5ca465def5b41f2d35f19067257ce807c6b9d84a
Carlos <carlos.cantrax303@gmail.com>
2024-01-27 11:23:54 -0600

    agrupacion imagenes

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -2,7 +2,7 @@
 @section('main')
     <div class="container mt-1">
         <div class="text-center">
-            <img src="{{ asset('/imagen/banner_unsis.png') }}" class="d-block mx-auto w-80" alt="Banner">
+            <img src="{{ asset('/imagen/img_inicio/banner_unsis.png') }}" class="d-block mx-auto w-80" alt="Banner">
             <h1 style="color: rgb(134, 40, 40)">Sistema de Permisos UNSIS</h1>
         </div>
     </div>
@@ -11,22 +11,22 @@
         <div id="miCarrusel" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img src="{{ asset('/imagen/A.JPG') }}" class="d-block w-100" alt="Imagen 1">
+                    <img src="{{ asset('/imagen/img_inicio/A.JPG') }}" class="d-block w-100" alt="Imagen 1">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('/imagen/B.JPG') }}" class="d-block w-100" alt="Imagen 2">
+                    <img src="{{ asset('/imagen/img_inicio/B.JPG') }}" class="d-block w-100" alt="Imagen 2">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('/imagen/A.JPG') }}" class="d-block w-100" alt="Imagen 3">
+                    <img src="{{ asset('/imagen/img_inicio/A.JPG') }}" class="d-block w-100" alt="Imagen 3">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('/imagen/C.JPG') }}" class="d-block w-100" alt="Imagen 4">
+                    <img src="{{ asset('/imagen/img_inicio/C.JPG') }}" class="d-block w-100" alt="Imagen 4">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('/imagen/F.JPG') }}" class="d-block w-100" alt="Imagen 5">
+                    <img src="{{ asset('/imagen/img_inicio/F.JPG') }}" class="d-block w-100" alt="Imagen 5">
                 </div>
                 <div class="carousel-item">
-                    <img src="{{ asset('/imagen/G.JPG') }}" class="d-block w-100" alt="Imagen 6">
+                    <img src="{{ asset('/imagen/img_inicio/G.JPG') }}" class="d-block w-100" alt="Imagen 6">
                 </div>
             </div>
 
