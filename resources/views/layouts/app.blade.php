b7a67bc8d6ff3e3b132384cca2fe458a4adfa7f8
Skar13F <of5162@gmail.com>
2024-02-01 11:01:27 -0600

    enlaces

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,30 +1,26 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app-name', 'Permisos Alumnos') }}</title>
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
 
-
     <!-- Font Awesome CSS -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" rel="stylesheet">
 
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
-    <!-- Para las tablas -->
+    <!-- DataTables CSS -->
     <link rel="stylesheet" href="{{ asset('css/datatables.min.css')}}">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
+    <!-- Estilos Personalizados -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -32,20 +28,18 @@
     <link href="{{ asset('css/secretaria.css') }}" rel="stylesheet">
     <script src="{{ asset('js/secretaria.js') }}"></script>
 
-
     <!-- login -->
     <link href="{{ asset('css/estilos.css') }}" rel="stylesheet">
 
     <!-- Estilos para las vistas de administrador -->
     <link href="{{ asset('css/administrador.css') }}">
-    <!-- Coloresbotones -->
+    <!-- Colores botones -->
     <link href="{{ asset('css/botones.css') }}" rel="stylesheet">
 
 </head>
 
 <body>
     <div id="app">
-
         @guest
         @else
             <!-- Section to show views and content -->
@@ -56,12 +50,14 @@
         <!-- Section to show login-->
         <main class="">
             @yield('login')
-
         </main>
     </div>
+
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <!-- Otros scripts que puedas tener -->
 
 </body>
 
