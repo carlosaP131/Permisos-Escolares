ed2eeb4f84ee79ebdbd225b4bd6ecbdf802db0e1
Skar13F <of5162@gmail.com>
2024-02-01 10:13:28 -0600

    datos

--- database/migrations/2024_01_17_141225_create_alumnos_table.php
+++ database/migrations/2024_01_17_141225_create_alumnos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('alumnos', function (Blueprint $table) {
-            $table->increments('id');
+            $table->unsignedBigInteger('id');
             $table->string('matricula');
             $table->string('nombre', 100);
             $table->string('apellido', 100);
